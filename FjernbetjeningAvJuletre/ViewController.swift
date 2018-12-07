7ddb45a2f4cc0f6a680e3ccbb0b28396289fc22d
Andreas <andreaswilson@client.openvpn.net>
2018-12-07 13:41:05 +0100

    Lagt til knapp for lys på/av

--- FjernbetjeningAvJuletre/ViewController.swift
+++ FjernbetjeningAvJuletre/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lysButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func lysOnClick(_ sender: Any) {
+        if(lysButton.currentTitle == "Lys på") {
+            lysButton.setTitle("Lys av", for: UIControlState.normal)
+        } else {
+            lysButton.setTitle("Lys på", for: UIControlState.normal)
+        }
+    }
 }
 
