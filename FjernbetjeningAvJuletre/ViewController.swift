828b6294415a02deef33b7798fa102f4b98b938a
Andreas <andreaswilson@client.openvpn.net>
2018-12-07 15:11:19 +0100

    Lagt til knapp for rotasjon av/på

--- FjernbetjeningAvJuletre/ViewController.swift
+++ FjernbetjeningAvJuletre/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var lysButton: UIButton!
+    @IBOutlet weak var rotasjonButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +22,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func rotasjonOnClick(_ sender: Any) {
+        switchTitle(button: rotasjonButton, currentTitle: "Rotasjon på", newTitle: "Rotasjon av")
+    }
     
+    //TODO:  Sender til arduino on lys skal være av eller på
     @IBAction func lysOnClick(_ sender: Any) {
-        if(lysButton.currentTitle == "Lys på") {
-            lysButton.setTitle("Lys av", for: UIControlState.normal)
+        switchTitle(button: lysButton, currentTitle: "Lys på", newTitle: "Lys av")
+    }
+    
+    private func switchTitle(button: UIButton, currentTitle: String, newTitle: String) {
+        if button.currentTitle == currentTitle {
+            button.setTitle(newTitle, for: UIControlState.normal)
         } else {
-            lysButton.setTitle("Lys på", for: UIControlState.normal)
+            button.setTitle(currentTitle, for: UIControlState.normal)
         }
     }
 }
